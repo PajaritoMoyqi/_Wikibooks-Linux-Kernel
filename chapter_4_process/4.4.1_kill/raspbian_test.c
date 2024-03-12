6f853fa9f67669f729482307c2a3341d1a41dc05
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2024-03-12 15:14:55 +0900

    [PROCESS] process kill example code (DONE)

---
+++ chapter_4_process/4.4.1_kill/raspbian_test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#define PROC_TIMES 500
+#define SLEEP_DURATION 3
+
+int main( void ) {
+	int proc_times;
+	for ( proc_times = 0; proc_times < PROC_TIMES; proc_times++ )
+	{
+		printf( "raspbian tracing\n" );
+		sleep( SLEEP_DURATION );
+	}
+
+	return 0;
+}
