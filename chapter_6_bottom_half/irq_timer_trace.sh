d0c777a8998deb883aa630a7123fd6c7e93809af
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2024-04-11 18:12:02 +0900

    [IRQ_THREAD] irq timer ftrace code (DONE)

--- chapter_6_bottom_half/irq_timer_trace.sh
+++ chapter_6_bottom_half/irq_timer_trace.sh
@@ -12,9 +12,9 @@ echo  secondary_start_kernel  > /sys/kernel/debug/tracing/set_ftrace_filter
 sleep 1
 echo "set_ftrace_filter init"
 
-echo function > /sys/kernel/debug/tracing/current_tracer
+echo function_graph > /sys/kernel/debug/tracing/current_tracer
 sleep 1
-echo "function tracer enabled"
+echo "function_graph tracer enabled"
 
 echo 1 > /sys/kernel/debug/tracing/events/irq/irq_handler_entry/enable
 echo 1 > /sys/kernel/debug/tracing/events/irq/irq_handler_exit/enable
@@ -24,12 +24,10 @@ sleep 1
 echo "event enabled"
 
 echo bcm2835_mmc_thread_irq bcm2835_mmc_irq > /sys/kernel/debug/tracing/set_ftrace_filter
+echo bcm2835_mbox_thread_irq bcm2835_mbox_irq >> /sys/kernel/debug/tracing/set_ftrace_filter
+echo bcm2835_sdhost_irq >> /sys/kernel/debug/tracing/set_ftrace_filter
 sleep 1
 echo "set_ftrace_filter enabled"
-
-echo 1 > /sys/kernel/debug/tracing/options/func_stack_trace
-echo 1 > /sys/kernel/debug/tracing/options/sym-offset
-echo "function stack trace enabled"
 
 echo 1 > /sys/kernel/debug/tracing/tracing_on
 echo "tracing_on"
